d553c93882aff436ebd1c40c5dfe86feecb2fffc
Antoine du FOU <antoine.du.fou@gmail.com>
2023-07-04 23:19:41 +0200

    UPDATE PACKAGE JSON + ESLINT + CONNFIG 2

--- front/src/components/Game/GameAddModal.jsx
+++ front/src/components/Game/GameAddModal.jsx
@@ -1,46 +1,64 @@
-import React, { useEffect, useState } from 'react';
-import { Input, Label, Button, TableContainer, Table, TableHeader, TableRow, TableCell, TableBody, Modal, ModalHeader, ModalBody, ModalFooter } from '@windmill/react-ui'
+import React, { useState } from 'react';
+import {
+    Input,
+    Label,
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+} from '@windmill/react-ui';
 import { useGameApi } from '../../http/api/game/useGameApi';
-import { useGameStore, useGlobalStore } from '../../stores/useGlobalStore';
+import { useGlobalStore } from '../../stores/useGlobalStore';
 import { addGame } from '../../stores/game/gameStoreActions';
 
-const GameAddModal = ({isOpen, closeModal}) => {
+const GameAddModal = ({ isOpen, closeModal }) => {
     const [newGameName, setGameName] = useState('');
 
-    const gameApi = useGameApi()
-    const store = useGlobalStore()
+    const gameApi = useGameApi();
+    const store = useGlobalStore();
 
     function addNewGame() {
         const newGame = {
-            name: newGameName
-        }
+            name: newGameName,
+        };
 
         gameApi.addGame(newGame, (response) => {
             if (response.status === 201) {
-                store.dispatch(addGame(response.body))
+                store.dispatch(addGame(response.body));
             }
-            
-            closeModal()
-        })
+
+            closeModal();
+        });
     }
 
     return (
         <Modal isOpen={isOpen} onClose={closeModal}>
-                <ModalHeader>Add a game</ModalHeader>
-                <ModalBody>
+            <ModalHeader>Add a game</ModalHeader>
+            <ModalBody>
                 <Label>
                     <span>Name</span>
-                    <Input className="mt-1" value={newGameName} onChange={e => setGameName(e.target.value)} />
+                    <Input
+                        className="mt-1"
+                        value={newGameName}
+                        onChange={(e) => setGameName(e.target.value)}
+                    />
                 </Label>
-                </ModalBody>
-                <ModalFooter>
-                    <Button className="w-full sm:w-auto" layout="outline" onClick={closeModal}>
-                        Cancel
-                    </Button>
-                    <Button className="w-full sm:w-auto" onClick={addNewGame}>Accept</Button>
-                </ModalFooter>
+            </ModalBody>
+            <ModalFooter>
+                <Button
+                    className="w-full sm:w-auto"
+                    layout="outline"
+                    onClick={closeModal}
+                >
+                    Cancel
+                </Button>
+                <Button className="w-full sm:w-auto" onClick={addNewGame}>
+                    Accept
+                </Button>
+            </ModalFooter>
         </Modal>
-    )
-}
+    );
+};
 
 export default GameAddModal;
