8db5b745f2b40a600a5d9cf5b9f72650d482ebd7
Antoine du FOU <antoine.du.fou@gmail.com>
2023-07-04 22:53:09 +0200

    UPDATE PACKAGE JSON + ESLINT + CONNFIG

--- front/src/stores/game/gameStoreImpl.js
+++ front/src/stores/game/gameStoreImpl.js
@@ -1,46 +1,48 @@
 export const addGame = (state, { game }) => {
-    return ({
-        ...state,
-        games: [
-            ...state.games,
-            {
-                ...game,
-            },
-        ]
-    })
-}
+	return ({
+		...state,
+		games: [
+			...state.games,
+			{
+				...game,
+			},
+		]
+	}   );
+
+
+};
 
 export const fetchGames = (state, { games }) => ({
-    ...state,
-    games: games
-})
+	...state,
+	games: games
+});
 
 export const deleteGame = (state, { game }) => {
-    const games = state.games;
-    const gameIndex = state.games.findIndex(g => g.id === game.id)
+	const games = state.games;
+	const gameIndex = state.games.findIndex(g => g.id === game.id);
 
-    console.log(gameIndex)
-    if (gameIndex !== -1) {
-        games.splice(gameIndex, 1);
-    }
+	console.log(gameIndex);
+	if (gameIndex !== -1) {
+		games.splice(gameIndex, 1);
+	}
 
-    return {
-        ...state,
-        games: games
-    }
-}
+	return {
+		...state,
+		games: games
+	};
+};
 
 export const updateGame = (state, { game }) => {
     
-    const games = state.games;
-    const gameIndex = state.games.findIndex(g => g.id === game.id)
+	const games = state.games;
+	const gameIndex = state.games.findIndex(g => g.id === game.id);
 
-    if (gameIndex !== -1) {
-        games[gameIndex] = game;
-    }
+	if (gameIndex !== -1) {
+		games[gameIndex] = game;
+	}
 
-    return {
-        ...state,
-        games: games,
-    }
-}
+	return {
+		...state,
+		games: games,
+	};
+};
