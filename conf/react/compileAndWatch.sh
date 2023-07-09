e180567252b1b065f0464d69e3bfa8492fd8ef54
adufou <antoine.du.fou@gmail.com>
2023-07-09 23:06:00 +0200

    create DockerFile for dev (default), save the prod one. Dev hot reload dont work

--- conf/react/compileAndWatch.sh
+++ conf/react/compileAndWatch.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-echo "a"
+# echo "a"
 
 # tsc-watch -p tsconfig.json &
 # cd ./front & webpack serve --watch
-tsc --watch -p tsconfig.json & 
-webpack serve --watch
+tsc-watch -p tsconfig.json 
+# tsc --watch -p tsconfig.json 
+# webpack serve --watch
