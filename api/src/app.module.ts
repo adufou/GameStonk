6edb2d1fb0afcb8de26d55a221b5c6b94bf6f2e8
Antoine du FOU <52314805+adufou@users.noreply.github.com>
2023-08-09 20:15:23 +0200

    Nestjs first steps (#24)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { GameModule } from './game/game.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -23,8 +25,10 @@ import {TypeOrmModule} from "@nestjs/typeorm";
           retryDelay: 5000,
           retryAttempts: 25,
       }),
+      GameModule,
+      UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
