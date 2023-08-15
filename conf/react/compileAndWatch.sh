a35dd955a6296b3b7dec186c5a58fc5388fb0d3f
Antoine du FOU <antoine.du.fou@gmail.com>
2023-08-15 03:44:38 +0200

    chore: :technologist: improve front Docker

--- conf/react/compileAndWatch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# echo "a"
-
-# tsc-watch -p front/tsconfig.json &
-# cd ./front & webpack serve --watch
-tsc-watch -p front/tsconfig.json 
-# tsc --watch -p front/tsconfig.json 
-# webpack serve --watch
