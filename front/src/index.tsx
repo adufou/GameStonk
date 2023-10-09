44bfea0a0b2ba63e0b3ba75e90dc8cb8be580118
Antoine du FOU <antoine.du.fou@gmail.com>
2023-10-09 23:47:19 +0200

    chore: :broom: clean and remove all old store leftovers

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -5,11 +5,9 @@ import {
     QueryClientProvider,
 } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@/components/App/App';
 import '@/index.scss';
-import store from '@/stores/globalStore';
 
 const root = document.getElementById('root');
 
@@ -17,13 +15,11 @@ const queryClient = new QueryClient();
 
 if (root) {
     createRoot(root).render(
-        <Provider store={store}>
-            <QueryClientProvider client={queryClient}>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-                <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
-        </Provider>,
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>,
     );
 }
--- front/src/stores/user/IUserState.ts
+++
@@ -1,8 +0,0 @@
-import UserModel from '@/models/user.model';
-
-interface IUserState {
-    user: UserModel | null;
-    token: string | null;
-}
-
-export default IUserState;
